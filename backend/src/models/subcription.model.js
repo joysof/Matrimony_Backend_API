bdb323fb3ff1edce2873cab0351cd28a81b57536
sajib hossen <sajibhossen560@gmail.com>
2025-10-13 23:50:35 +0600

    subctiption model create

---
+++ backend/src/models/subcription.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const subscriptionSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Subscription name is required'],
+      trim: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    days: {
+      type: Number,
+      required: true,
+    },
+    feature: {
+      type: [String],
+      default: [],
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'active',
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema)
+
+module.exports = Subscription
