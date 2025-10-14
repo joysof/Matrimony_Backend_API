0858b0476ffb3398521f0d8d1c6ccfbe86dc0485
sajib hossen <sajibhossen560@gmail.com>
2025-10-14 13:13:32 +0600

    mySubcription models create

---
+++ backend/src/models/mySubcription.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+
+
+
+
+const mySubcriptionSchema = mongoose.Schema({
+    subcriptionId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Subscription',
+        required : true
+    },
+    userId:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    expiresDate :{
+        type: Date,
+        required:true
+    },
+    name:{
+        type:String,
+        requried:true
+    },
+    duration:{
+        type:String,
+        required:true
+    },
+    price : {
+        type:Number,
+        required:true
+    },
+    sendReminder : {
+        type:String,
+        required:true,
+        default: 0
+    },
+    sendMessage : {
+        type:Number,
+        required: true,
+        default: 0
+    },
+    matchesMessageLimit:{
+        type: Number,
+        required:true,
+        default: 0
+    }
+})
+
+
+const mySubcription = mongoose.model('mySubcription' , mySubcriptionSchema)
+
+module.exports = mySubcription
